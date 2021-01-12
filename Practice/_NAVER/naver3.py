21621bdea34e0e45a5502c8c1ee506b85a00036f
osj3474 <handong3474@gmail.com>
2021-01-12 13:51:50 +0900

    delete test

--- Practice/_NAVER/naver3.py
+++
@@ -1,6 +0,0 @@
-from itertools import combinations
-
-a=[2,3,4,5,6,7]
-for i in range(2,50):
-    b = list(combinations(a, i))
-    print(b)
--- Practice/_brandi/brandi3.py
+++
@@ -1,5 +0,0 @@
-N, k = map(int, input().split())
-strength_lst = list(map(int, input().split()))
-path_lst = [list(map(int, input().split())) for _ in range(k)]
-
-print(N, k, strength_lst, path_lst)
