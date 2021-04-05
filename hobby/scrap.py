aff4afb007d1a11068e5a5b3d4b5657aa94e45dc
osj3474 <handong3474@gmail.com>
2021-04-05 22:55:48 +0900

    Update open kakao

--- hobby/scrap.py
+++ hobby/scrap.py
@@ -2,13 +2,41 @@
 # pip3 install Image
 # pip3 install pywinauto
 
+
 import pyautogui
+import time
+import pyperclip
 
 
-kakao = pyautogui.locateOnScreen('/Users/sangjin/Desktop/osj/Algorithm-Practice/hobby/chat1.png')
-print(kakao)
+# kakao = pyautogui.locateOnScreen('/Users/sangjin/Desktop/osj/Algorithm-Practice/hobby/kakao.png')
+# print(kakao)
+#
+# pyautogui.click(kakao)
+# pyautogui.click(kakao)
 
-pyautogui.click(kakao)
-pyautogui.click(kakao)
+pyautogui.click()
+time.sleep(0.5)
+pyautogui.hotkey('command', 'space')
+pyautogui.hotkey('backspace')
+time.sleep(0.5)
+pyperclip.copy('kakao')
+pyautogui.hotkey("command", "v")
+time.sleep(0.5)
+pyautogui.hotkey('enter')
+time.sleep(0.5)
+pyautogui.hotkey('command', "2")
+time.sleep(0.5)
+pyautogui.hotkey('command', 'f')
+pyautogui.hotkey('backspace')
+time.sleep(1)
+pyperclip.copy('오상진')
+pyautogui.hotkey("command", "v")
+time.sleep(0.5)
+pyautogui.hotkey('down')
+time.sleep(0.5)
+pyautogui.hotkey('enter')
+time.sleep(0.5)
+ham = pyautogui.locateOnScreen('/Users/sangjin/Desktop/osj/Algorithm-Practice/hobby/kakao.png')
+pyautogui.click(ham)
 
-pyautogui.typewrite('오상진', interval=0.1)
+
