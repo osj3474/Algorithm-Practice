061975ef745c9cc2462715bf75976edb59166631
osj3474 <handong3474@gmail.com>
2021-04-24 15:17:21 +0900

    Update baeck11052

--- BackToBasic/baek11052.py
+++ BackToBasic/baek11052.py
@@ -1,29 +1,9 @@
 N = int(input())
 card_lst = [0]+list(map(int, input().split()))
 
-def buy(lst, n):
-    # 0보다 작은 경우 => fail
-    if n<0:
-        return False
-    # 0인 경우 => finished
-    if n==0:
-        return True
-    # 0보다 큰 경우 => 재귀
-    if n>0:
-        return buy(lst, n)
-
-# 가능한 조합
-# n=4일 경우,
-# 1111, 112, 13
-# 22
-# 4
-
-# n=5일 경우,
-# 11111, 1112, 113, 122, 14,
-#
 
 # D[n] = N개를 갖기위해 지불해야하는 금액의 최댓값
-#      =
+
 memo = [0]*(N+1)
 for i in range(1, N+1):
     for j in range(1, i+1):
