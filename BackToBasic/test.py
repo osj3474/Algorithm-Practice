5343f73c51deb7d8f844d4f96fc77bda8b2bda2f
osj3474 <handong3474@gmail.com>
2021-05-12 21:50:51 +0900

    Try 1932

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -1,30 +1,16 @@
-n = int(input())
-A = list(map(int, input().split()))
+N = int(input())
+A = [list(map(int, input().split())) for _ in range(N)]
 
-MAX = temp = A[0]
-out = None
-flag = True
-cnt = 0
-for i in range(1, len(A)):
-    if A[i]<0:
-        cnt += 1
-        if out:
-            if flag:
-                temp += A[out]
-            else:
-                temp = sum(A[out+1:i])
-        else:
-            if temp+A[i]<0:
-                flag =False
-        out = i
-        continue
-    if temp+A[i] < 0:
-        temp = 0
-        continue
-    temp += A[i]
-    MAX = max(MAX, temp)
-
-if cnt == len(A):
-    print(min(A))
-else:
-    print(MAX)
+length = 1
+past = [A[0][0]]
+for i in range(1, N):
+    size = length*2
+    past = new
+    new = [0]*size
+    for p in past:
+        new[0] = past[p]+A[i][0]
+        new[-1] = p+A[i][-1]
+        k = 1
+        for j in range(1, size-2, 2):
+            new[j] = p+A[i][k]
+            new[j+1] = p+A[i][k]
