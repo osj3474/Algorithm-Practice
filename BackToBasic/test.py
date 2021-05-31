d9e1bb49948a554616b28123b4cfc660f51807df
osj3474 <handong3474@gmail.com>
2021-05-31 23:06:04 +0900

    Try 1697.py

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -1,41 +1,18 @@
-N_str = input()
-M = int(input())
-A_set = set(map(int, input().split()))
-A_lst = list(set([i for i in range(10)]) - A_set)
-A_lst.sort()
+from collections import deque
 
-def find_nearest(n):
-    MIN = 10
-    ret = 0
-    for i in range(len(A_lst)):
-        temp = abs(A_lst[i]-n)
-        if MIN > temp:
-            MIN = temp
-            ret = A_lst[i]
-    return ret
+N, K = 5, 17
+need = deque()
+need.append(N)
 
-s_idx = 0
-num = ''
-
-if A_lst[0]==0:
-    MIN = 10
-    ret = 0
-    k = int(N_str[0])
-    for i in range(1, len(A_lst)):
-        temp = abs(A_lst[i]-k)
-        if MIN > temp:
-            MIN = temp
-            ret = A_lst[i]
-    num += str(ret)
-    s_idx = 1
-
-for i in range(s_idx, len(N_str)):
-    num += str(find_nearest(int(N_str[i])))
-
-answer = len(N_str)+abs(int(N_str)-int(num))
-print(min(answer, abs(100-int(N_str))))
-
-# 와,,, 반례,,, 정말 생각지도 못한,,,
-#1555
-#8
-#0 1 3 4 5 6 7 9
+visited = list()
+cnt = 0
+while need:
+    if K in need: break
+    new = list()
+    for item in list(need):
+        n = need.popleft()
+        if n in visited: continue
+        new.extend([n-1, n+1, n*2])
+    need = deque(new)
+    cnt += 1
+print(cnt)
