24eceaae2e82d5f23d0b6ce288f74dfb57d31303
osj3474 <handong3474@gmail.com>
2021-05-31 23:11:57 +0900

    Fix input

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-N, K = 5, 17
+N, K = map(int, input().split())
 need = deque()
 need.append(N)
 
@@ -12,6 +12,7 @@ while need:
     for item in list(need):
         n = need.popleft()
         if n in visited: continue
+        visited.extend([n-1, n+1, n*2])
         new.extend([n-1, n+1, n*2])
     need = deque(new)
     cnt += 1
