55d32610f5d9e31b6b884408cebc4c540456bba7
osj3474 <handong3474@gmail.com>
2021-07-11 22:45:43 +0900

    Success : my own bfs

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -14,10 +14,10 @@ class Node:
     def add_lst(self, ele):
         self.prev_lst.append(ele)
 
-board=['...','##.','...']
+board=['...','#.#','...']
 
 dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+dy = [0, -1, 0, 1]
 
 # DS, BQ
 # 최소니까 BQ로 해야한다.
@@ -28,31 +28,25 @@ N, M = 3, 3
 srcX, srcY, destX, destY = 0, 0, 2, 0
 
 visited = list()
-route.append(Node(srcX, srcY, list()))
-# visited.append(Node(srcX, srcY))
+route.append(Node(srcX, srcY, [(srcX, srcY)]))
 breakFlag=False
-cnt = 0
 
 while route:
     node = route.popleft()
     i, j = node.get_xy()
-    finFlag = False
+    prev_lst = node.prev_lst
+    print("{}, {}노드를 할꺼구, 지나온 길은 {}야.".format(i, j, node.prev_lst))
     for p in range(4):
         x = i+dx[p]
         y = j+dy[p]
         if x >= M or x < 0 or y >= N or y < 0 or ((x, y) in node.prev_lst): continue
         if board[x][y]=='.':
-            node.add_lst((x, y))
             if (x, y) == (destX, destY):
+                node.add_lst((x, y))
                 breakFlag = True; break
-            route.append(Node(x, y, node.prev_lst))
+            print("     지금 {}, {}에서 {}리스트가 추가될거야.".format(x, y, prev_lst+[(x,y)]))
+            route.append(Node(x, y, prev_lst+[(x,y)]))
     if breakFlag: break
 
 print(node.prev_lst)
-
-
-
-
-
-
 
