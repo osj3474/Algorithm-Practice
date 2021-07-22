4d0a9b4469482d53da6d64e40b8ac9ce0de447cf
osj3474 <handong3474@gmail.com>
2021-07-22 16:01:20 +0900

    Practice Dijstra

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -1,33 +1,68 @@
-# 위상정렬이 이런거구나.
-# DAG = 단방향 no사이클인 그래프에서
-# 순서를 지정하는거
+# N = int(input())
+# M = int(input())
+# bus_lst = [list(map(int, input().split())) for _ in range(M)]
+# S, T = map(int, input().split())
+#
+# edge_dic = dict()
+# for i in range(1, N+1):
+#     edge_dic[i]=dict()
+#
+# for bus in bus_lst:
+#     start, end, price = bus[0], bus[1], bus[2]
+#     edge_dic[start].update({end:price})
+#
+# print(edge_dic)
 
-from collections import deque
-from collections import defaultdict
 
-N, M = map(int, input().split())
-edge_lst = [list(map(int, input().split())) for _ in range(M)]
+N = int(input())
+M = int(input())
+bus_lst = [list(map(int, input().split())) for _ in range(M)]
+s, t = map(lambda x:int(x)-1, input().split())
 
-node_dic = defaultdict(list)
-inbound_lst = [0]*(N+1)
-queue = deque()
-result = list()
 
-for e in edge_lst:
-    node_dic[e[0]].append(e[1])
-    inbound_lst[e[1]]+=1
+inf = float('inf')
+board = [[-1]*N for _ in range(N)]
 
-for i in range(1, len(inbound_lst)):
-    if inbound_lst[i]==0:
-        queue.append(i)
+for bus in bus_lst:
+    board[bus[0]-1][bus[1]-1] = bus[2]
 
-while queue:
-    now = queue.popleft()
-    for i in node_dic[now]:
-        inbound_lst[i] -= 1
-        if inbound_lst[i]==0: queue.append(i)
-    result.append(now)
-    node_dic[now] = []
+for i in range(N):
+    for j in range(N):
+        if i==j: board[i][j]=0; continue
+        if board[i][j]==-1: board[i][j]=inf
 
-for i in list(result):
-    print(i, end=' ')
+
+
+# board = [
+#     [0, 2, 5, 1, inf, inf],
+#     [2, 0, 3, 2, inf, inf],
+#     [5, 3, 0, 3, 1, 5],
+#     [1, 2, 3, 0, 1, inf],
+#     [inf, inf, 1, 1, 0, 2],
+#     [inf, inf, 5, inf, 2, 0]
+# ]
+v = [False]*N
+d = [0]*N
+
+def getSmallIndex():
+    min = float('inf')
+    idx = 0
+    for i in range(N):
+        if d[i]<min and not v[i]:
+            min = d[i]
+            idx = i
+    return idx
+
+for i in range(N):
+    d[i] = board[s][i]
+v[s] = True
+for i in range(N-2):
+    cur = getSmallIndex()
+    v[cur] = True
+    for j in range(N):
+        if not v[j]:
+            if d[cur]+board[cur][j] < d[j]:
+                d[j] = d[cur]+board[cur][j]
+
+print(d[t])
+
