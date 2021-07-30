a32e67d3fb0918688384690e2c23b89ce25c2e06
osj3474 <handong3474@gmail.com>
2021-07-30 22:05:13 +0900

    Update try

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -7,51 +7,65 @@ dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 
 MAX=0
-origin_dic = {'C':0, 'P':0, 'Z':0, 'Y':0}
-for i in range(N):
-    width_dic = dc(origin_dic)
-    height_dic = dc(origin_dic)
-    for j in range(N):
-        width_dic[board[i][j]]+=1
-        height_dic[board[j][i]]+=1
-    temp = max(width_dic.values())
-    if MAX<temp: MAX=temp
-    temp = max(height_dic.values())
-    if MAX < temp: MAX = temp
+
+def getRowMAX(board, N, r):
+    MAX = 0
+    prev = board[r][0]
+    cnt = 1
+    for i in range(1, N):
+        cur = board[r][i]
+        if prev==cur:
+            cnt+=1
+            if MAX < cnt: MAX = cnt
+        else:
+            prev = cur
+            if MAX < cnt: MAX = cnt
+            cnt = 1
+    return MAX
+
+def getColMAX(board, N, c):
+    MAX = 0
+    prev = board[0][c]
+    cnt = 1
+    for i in range(1, N):
+        cur = board[i][c]
+        if prev==cur:
+            cnt+=1
+            if MAX < cnt: MAX = cnt
+        else:
+            prev = cur
+            if MAX < cnt: MAX = cnt
+            cnt = 1
+    return MAX
+
+# board[2][1], board[2][2] = board[2][2], board[2][1]
+# print(getColMAX(board,N,2))
+
+# for i in range(N):
+#     MAX = max(MAX, getRowMAX(board, N, i), getColMAX(board, N, i))
 
 for i in range(N):
     for j in range(N):
-        # print('{}, {} 차례다'.format(i, j))
+        print('{}, {} 차례다'.format(i, j))
         for p in range(4):
             x = i + dx[p]
             y = j + dy[p]
             if x>=N or y>=N or x<0 or y<0: continue
             if board[i][j]==board[x][y]: continue
             board[i][j], board[x][y] = board[x][y], board[i][j]
-            if i==x:
-                # print('{}열 모두 검사'.format(j))
-                # print('{}열 모두 검사'.format(y))
-                temp1_dic = dc(origin_dic)
-                temp2_dic = dc(origin_dic)
-                for k in range(N):
-                    temp1_dic[board[k][j]] += 1
-                    temp2_dic[board[k][y]] += 1
-                temp = max(temp1_dic.values())
-                if MAX < temp: MAX = temp
-                temp = max(temp2_dic.values())
-                if MAX < temp: MAX = temp
-            if j==y:
-                # print('{}행 모두 검사'.format(i))
-                # print('{}행 모두 검사'.format(x))
-                temp1_dic = dc(origin_dic)
-                temp2_dic = dc(origin_dic)
-                for k in range(N):
-                    temp1_dic[board[i][k]] += 1
-                    temp2_dic[board[x][k]] += 1
-                temp = max(temp1_dic.values())
-                if MAX < temp: MAX = temp
-                temp = max(temp2_dic.values())
-                if MAX < temp: MAX = temp
+            for now in range(N):
+                print(board[now])
+            for k in range(N):
+                MAX = max(getColMAX(board, N, k), getColMAX(board, N, k))
             board[i][j], board[x][y] = board[x][y], board[i][j]
 
 print(MAX)
+
+
+class Person():
+    def __init__(self, count):
+        self.count = count
+    def addCnt(self):
+        self.count += 1
+
+
