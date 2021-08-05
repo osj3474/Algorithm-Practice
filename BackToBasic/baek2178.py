8cdf8576e44ca84010ef85b88b8db87416f95d18
osj3474 <handong3474@gmail.com>
2021-08-05 22:45:44 +0900

    Update 2178

---
+++ BackToBasic/baek2178.py
@@ -0,0 +1,91 @@
+# N, M = map(int, input().split())
+# board = [list(map(lambda x:int(x), input())) for _ in range(N)]
+#
+# from collections import deque
+# from copy import deepcopy
+#
+# class Node:
+#     def __init__(self, x, y, cnt):
+#         self.x = x
+#         self.y = y
+#         self.prev = set()
+#         self.cnt = cnt
+#     def getInfo(self):
+#         return self.x, self.y, self.prev, self.cnt
+#
+# dx = [1, 0, -1, 0]
+# dy = [0, 1, 0, -1]
+#
+# need = deque()
+# need.append(Node(0, 0, 1))
+# destX, destY = N-1, M-1
+# check_lst = [[0]*M for _ in range(N)]
+#
+# while need:
+#     node = need.popleft()
+#     i, j, prev, cnt = node.getInfo()
+#     prev_length = len(prev)
+#     if check_lst[i][j] != 0 and check_lst[i][j] < prev_length: continue
+#     print('[{}] {}, {}를 하고, prev={}, cnt={}입니다.'.format(cnt, i, j, prev, cnt))
+#     if (i, j)==(destX, destY):
+#         print("종료되었습니다.")
+#         break
+#     if board[i][j]==0:
+#         print('   {}, {}는 0이라 못가요.'.format(i, j))
+#         continue
+#     for p in range(4):
+#         x=i+dx[p]
+#         y=j+dy[p]
+#         if x>=N or y>=M or x<0 or y<0 or (x, y) in prev: continue
+#         if board[x][y]!=0:
+#             temp = deepcopy(prev)
+#             temp.add((i, j))
+#             print('       다음 스텝으로 {}, {}를 넣습니다. 직전 친구는 {}입니다.'.format(x, y, temp))
+#             new = Node(x, y, cnt + 1)
+#             new.prev = temp
+#             check_lst[x][y]=len(temp)
+#             print('       그리고 check[{}][{}]리스트에는 {}가 들어갔습니다.'.format(x, y, check_lst[x][y]))
+#             need.append(new)
+#
+# print(check_lst)
+# print(cnt)
+#
+# # 2178
+
+
+from collections import deque
+
+N, M = map(int, input().split())
+board = [input() for _ in range(N)]
+
+cnt = 1
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+# limit = 20
+dist = [[0]*M for _ in range(N)]
+visited = [[False]*M for _ in range(N)]
+
+need = deque()
+need.append((0, 0))
+dist[0][0] = 1
+visited[0][0] = True
+while need:
+    now = need.popleft()
+    i, j = now
+    for p in range(4):
+        x = i + dx[p]
+        y = j + dy[p]
+        if x >= 0 and y >= 0 and x < N and y < M:
+            if board[x][y] == '0' or visited[x][y]==True: continue
+            need.append((x, y))
+            dist[x][y] = dist[i][j] + 1
+            visited[x][y] = True
+            # print('{}, {} 는 방문을 햇습니당~'.format(x, y))
+            # limit -= 1
+            # if limit<0: break
+    # if limit < 0: break
+
+
+print(dist[N-1][M-1])
--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -1,71 +1,36 @@
-from copy import deepcopy as dc
+from collections import deque
 
-N = int(input())
-board = [list(map(lambda x:x, input())) for _ in range(N)]
+N, M = map(int, input().split())
+board = [input() for _ in range(N)]
+
+cnt = 1
 
 dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
+dy = [0, 1, 0, -1]
 
-MAX=0
+# limit = 20
+dist = [[0]*M for _ in range(N)]
+visited = [[False]*M for _ in range(N)]
 
-def getRowMAX(board, N, r):
-    MAX = 0
-    prev = board[r][0]
-    cnt = 1
-    for i in range(1, N):
-        cur = board[r][i]
-        if prev==cur:
-            cnt+=1
-            if MAX < cnt: MAX = cnt
-        else:
-            prev = cur
-            if MAX < cnt: MAX = cnt
-            cnt = 1
-    return MAX
-
-def getColMAX(board, N, c):
-    MAX = 0
-    prev = board[0][c]
-    cnt = 1
-    for i in range(1, N):
-        cur = board[i][c]
-        if prev==cur:
-            cnt+=1
-            if MAX < cnt: MAX = cnt
-        else:
-            prev = cur
-            if MAX < cnt: MAX = cnt
-            cnt = 1
-    return MAX
-
-# board[2][1], board[2][2] = board[2][2], board[2][1]
-# print(getColMAX(board,N,2))
-
-# for i in range(N):
-#     MAX = max(MAX, getRowMAX(board, N, i), getColMAX(board, N, i))
-
-for i in range(N):
-    for j in range(N):
-        print('{}, {} 차례다'.format(i, j))
-        for p in range(4):
-            x = i + dx[p]
-            y = j + dy[p]
-            if x>=N or y>=N or x<0 or y<0: continue
-            if board[i][j]==board[x][y]: continue
-            board[i][j], board[x][y] = board[x][y], board[i][j]
-            for now in range(N):
-                print(board[now])
-            for k in range(N):
-                MAX = max(getColMAX(board, N, k), getColMAX(board, N, k))
-            board[i][j], board[x][y] = board[x][y], board[i][j]
-
-print(MAX)
+need = deque()
+need.append((0, 0))
+dist[0][0] = 1
+visited[0][0] = True
+while need:
+    now = need.popleft()
+    i, j = now
+    for p in range(4):
+        x = i + dx[p]
+        y = j + dy[p]
+        if x >= 0 and y >= 0 and x < N and y < M:
+            if board[x][y] == '0' or visited[x][y]==True: continue
+            need.append((x, y))
+            dist[x][y] = dist[i][j] + 1
+            visited[x][y] = True
+            # print('{}, {} 는 방문을 햇습니당~'.format(x, y))
+            # limit -= 1
+            # if limit<0: break
+    # if limit < 0: break
 
 
-class Person():
-    def __init__(self, count):
-        self.count = count
-    def addCnt(self):
-        self.count += 1
-
-
+print(dist[N-1][M-1])
