7dc42d5e651c9f4e66d99c9488f55e8bb51a1b6d
osj3474 <handong3474@gmail.com>
2021-08-07 17:56:57 +0900

    Update 1463

--- BackToBasic/baek1463.py
+++ BackToBasic/baek1463.py
@@ -6,24 +6,38 @@ from collections import defaultdict
 # 점화식 => D[N]=min(D[N-1], D[N//2], D[N//3])+1
 # 구현   => 재귀
 
-def operate(n, memo):  # return: 최솟값
-    # base
-    if n==1:
-        return 0
-    # memorization
-    if memo[n] > 0:
-        return memo[n]
+# def operate(n, memo):  # return: 최솟값
+#     # base
+#     if n==1:
+#         return 0
+#     # memorization
+#     if memo[n] > 0:
+#         return memo[n]
+#
+#     # recursive
+#     memo[n] = operate(n-1, memo)+1
+#     if n%2==0:
+#         temp = operate(n//2, memo)+1
+#         memo[n] = min(memo[n], temp)
+#     if n%3==0:
+#         temp = operate(n//3, memo)+1
+#         memo[n] = min(memo[n], temp)
+#     return memo[n]
+#
+# n = int(input())
+# memo = [0]*(n+1)
+# print(operate(n, memo))
 
-    # recursive
-    memo[n] = operate(n-1, memo)+1
-    if n%2==0:
-        temp = operate(n//2, memo)+1
-        memo[n] = min(memo[n], temp)
-    if n%3==0:
-        temp = operate(n//3, memo)+1
-        memo[n] = min(memo[n], temp)
-    return memo[n]
 
-n = int(input())
-memo = [0]*(n+1)
-print(operate(n, memo))
+N = int(input())
+dp = [0]*(N+1)
+dp[1] = 0
+
+for i in range(2, N+1):
+    lst = []
+    if i%3==0: lst.append(dp[i//3])
+    if i%2==0: lst.append(dp[i//2])
+    lst.append(dp[i-1])
+    dp[i]=min(lst)+1
+
+print(dp[N])
--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -1,13 +1,49 @@
+# # #1890
+# # # DFS/BFS
+# N = int(input())
+# board = [list(map(int, input().split())) for _ in range(N)]
+#
+# dp = [[0]*N for _ in range(N)]
+# need, prev = [], []
+# need.append((0,0))
+# d = N-1
+#
+# cnt = 0
+# while need:
+#     now = need.pop()
+#     i, j = now
+#     if (i, j) != (0,0):
+#         prev.append(now)
+#     if dp[i][j]==1:
+#         cnt += 1
+#         continue
+#     if (i, j)== (d, d):
+#         cnt += 1
+#         for p1, p2 in prev:
+#             dp[p1][p2] = 1
+#         prev = []
+#         continue
+#     dx = [board[i][j], 0]
+#     dy = [0, board[i][j]]
+#     for p in range(2):
+#         x = i+dx[p]
+#         y = j+dy[p]
+#         if x>=0 and y>=0 and x<N and y<N:
+#             need.append((x, y))
+#
+# print(cnt)
+
+
 N = int(input())
-cnt = 0
+dp = [0]*(N+1)
+dp[1] = 0
 
-while N:
-    if N%5==0:
-        cnt += N//5; break
-    elif N>=3:
-        cnt += 1
-        N -= 3
-    else:
-        cnt = -1; break
+for i in range(2, N+1):
+    lst = []
+    if i%3==0: lst.append(dp[i//3])
+    if i%2==0: lst.append(dp[i//2])
+    lst.append(dp[i-1])
+    dp[i]=min(lst)+1
 
-print(cnt)
+print(dp[N])
+
