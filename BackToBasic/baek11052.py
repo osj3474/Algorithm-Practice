dbafb5cba8b8f54ab9100eecad7454e5922b3666
osj3474 <handong3474@gmail.com>
2021-08-08 07:47:36 +0900

    Update 11052

--- BackToBasic/baek11052.py
+++ BackToBasic/baek11052.py
@@ -10,3 +10,19 @@ for i in range(1, N+1):
         memo[i] = max(memo[i], memo[i-j]+card_lst[j])
 
 print(memo[N])
+
+
+
+# 다시 풀기
+# dp[n] = n개 카드 구매 최대 비용
+# = max(dp[n-1]+p[1], dp[n-2]+p[2], ...)
+#
+# N = int(input())
+# p_lst = [0]+list(map(int, input().split()))
+#
+# dp = [0]*(N+1)
+# dp[1] = p_lst[1]
+# for i in range(2, N+1):
+#     dp[i] = max([dp[i-j]+p_lst[j] for j in range(1, i)]+[p_lst[i]])
+#
+# print(dp[N])
--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -33,3 +33,15 @@
 #
 # print(cnt)
 
+# dp[n] = n개 카드 구매 최대 비용
+# = max(dp[n-1]+p[1], dp[n-2]+p[2], ...)
+
+N = int(input())
+p_lst = [0]+list(map(int, input().split()))
+
+dp = [0]*(N+1)
+dp[1] = p_lst[1]
+for i in range(2, N+1):
+    dp[i] = max([dp[i-j]+p_lst[j] for j in range(1, i)]+[p_lst[i]])
+
+print(dp[N])
