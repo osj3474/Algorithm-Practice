cfe1f50c71e0afe8e8d3feb20302a913dcfcfe24
osj3474 <handong3474@gmail.com>
2021-08-08 23:12:08 +0900

    Update 14501

--- BackToBasic/baek14501.py
+++ BackToBasic/baek14501.py
@@ -31,38 +31,58 @@ task_lst = [list(map(int, input().split())) for _ in range(N)]
 #     if MAX < value:
 #         MAX = value
 #
-from collections import defaultdict
+# from collections import defaultdict
+#
+# def consult(lst, visited=[]):
+#     global memo
+#     if not lst:
+#         return visited
+#     idx, value = lst[0]
+#     if (idx, value) in memo:
+#         visited.append((idx, value))
+#         return visited
+#     length = len(lst)
+#     print()
+#     if length-idx < 0:
+#         memo[(idx, value)] = 0
+#         return visited
+#     memo[(idx, value)] = value
+#     visited.append((idx, value))
+#     for i in range(idx, length):
+#         result = consult(lst[idx:], visited)
+#         total = 0
+#         print("result: ", result)
+#         print()
+#         # for key in result:
+#         #     total += memo[key]
+#         # print(total)
+#
+#
+# n = task_lst[0][0]
+# global memo
+# memo=defaultdict(int)
+#
+# for i in range(n):
+#     consult(task_lst[i:])
+#
+# print(memo)
 
-def consult(lst, visited=[]):
-    global memo
-    if not lst:
-        return visited
-    idx, value = lst[0]
-    if (idx, value) in memo:
-        visited.append((idx, value))
-        return visited
-    length = len(lst)
-    print()
-    if length-idx < 0:
-        memo[(idx, value)] = 0
-        return visited
-    memo[(idx, value)] = value
-    visited.append((idx, value))
-    for i in range(idx, length):
-        result = consult(lst[idx:], visited)
-        total = 0
-        print("result: ", result)
-        print()
-        # for key in result:
-        #     total += memo[key]
-        # print(total)
+N = int(input())
+t, p = [0]*(N+1), [0]*(N+1)
+for i in range(1, N+1):
+    t[i], p[i] = map(int, input().split())
 
+global MAX
+MAX = 0
 
-n = task_lst[0][0]
-global memo
-memo=defaultdict(int)
+def consult(d, s):
+    global MAX
+    if d==N+1:
+        if MAX < s: MAX = s
+        return
+    if d>N+1: return
+    consult(d+t[d], s+p[d])
+    consult(d+1, s)
 
-for i in range(n):
-    consult(task_lst[i:])
-
-print(memo)
+consult(1, 0)
+print(MAX)
