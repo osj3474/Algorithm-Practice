1a16ddcfce82d7e4c59a8ba11df3c72c8b4e7443
osj3474 <handong3474@gmail.com>
2021-08-08 17:27:22 +0900

    Update 1912

--- BackToBasic/baek1912.py
+++ BackToBasic/baek1912.py
@@ -1,18 +1,28 @@
+# n = int(input())
+# A = list(map(int, input().split()))
+#
+# MAX, sub_s = 0, 0
+# flag = False
+# for i in range(len(A)):
+#     if A[i] > 0: flag = True
+#     if sub_s+A[i] > 0:
+#         sub_s += A[i]
+#         if MAX < sub_s:
+#             MAX = sub_s
+#     else:
+#         sub_s = 0
+#
+# if flag:
+#     print(MAX)
+# else:
+#     print(max(A))
+
 n = int(input())
-A = list(map(int, input().split()))
+lst = list(map(int, input().split()))
 
-MAX, sub_s = 0, 0
-flag = False
-for i in range(len(A)):
-    if A[i] > 0: flag = True
-    if sub_s+A[i] > 0:
-        sub_s += A[i]
-        if MAX < sub_s:
-            MAX = sub_s
-    else:
-        sub_s = 0
+dp = [0]*n
+dp[0]=lst[0]
+for i in range(1, len(lst)):
+    dp[i] = max(dp[i-1]+lst[i], lst[i])
 
-if flag:
-    print(MAX)
-else:
-    print(max(A))
+print(max(dp))
--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -48,37 +48,3 @@
 # lst = list(cb(dp, 3))
 # print(lst)
 
-N = int(input())
-A_lst = list(map(int, input().split()))
-
-dp = [[1,i] for i in range(N)]
-MAX = 0
-MAX_idx = 0
-
-for i in range(N):
-    temp = 0
-    flag = False
-    idx = i
-    for j in range(i):
-        if A_lst[i] > A_lst[j] and temp < dp[j][0]:
-            temp = dp[j][0]
-            idx = j
-            flag = True
-    if flag: dp[i] = [temp+1, idx]
-    if MAX < dp[i][0]:
-        MAX = dp[i][0]
-        MAX_idx = i
-
-value = 0
-visited_lst = []
-idx = MAX_idx
-while value != 1:
-    visited_lst.append(A_lst[idx])
-    value, idx = dp[idx]
-
-visited_lst.reverse()
-print(MAX)
-for visit in visited_lst:
-    print(visit, end=' ')
-
-
