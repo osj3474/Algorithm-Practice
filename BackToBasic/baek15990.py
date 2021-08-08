460f8ac520729725b546938990048897715375dd
osj3474 <handong3474@gmail.com>
2021-08-08 09:02:37 +0900

    Update 15990

--- BackToBasic/baek15990.py
+++ BackToBasic/baek15990.py
@@ -5,3 +5,22 @@
 # D[n][1] = D[n-1][2] + D[n-1][3]
 # D[n][2] = D[n-2][1] + D[n-2][3]
 # D[n][3] = D[n-3][1] + D[n-3][2]
+
+n = int(input())
+num_lst = [int(input()) for _ in range(n)]
+MAX = max(num_lst)
+k = 1000000009
+dp = [[0]*4 for _ in range(MAX+1)]
+dp[1][1] = 1
+dp[2][2] = 1
+dp[3][1] = 1
+dp[3][2] = 1
+dp[3][3] = 1
+
+for i in range(4, MAX+1):
+    dp[i][1] = dp[i-1][2]%k+dp[i-1][3]%k
+    dp[i][2] = dp[i-2][1]%k+dp[i-2][3]%k
+    dp[i][3] = dp[i-3][1]%k+dp[i-3][2]%k
+
+for i in num_lst:
+    print((dp[i][1]+dp[i][2]+dp[i][3])%k)
--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -36,3 +36,22 @@
 # dp[n] = n개 카드 구매 최대 비용
 # = max(dp[n-1]+p[1], dp[n-2]+p[2], ...)
 
+# dp[i][j] = i를 1,2,3의 합으로 나타내는 경우의 수, 마지막 사용한 수 j
+n = int(input())
+num_lst = [int(input()) for _ in range(n)]
+MAX = max(num_lst)
+k = 1000000009
+dp = [[0]*4 for _ in range(MAX+1)]
+dp[1][1] = 1
+dp[2][2] = 1
+dp[3][1] = 1
+dp[3][2] = 1
+dp[3][3] = 1
+
+for i in range(4, MAX+1):
+    dp[i][1] = dp[i-1][2]%k+dp[i-1][3]%k
+    dp[i][2] = dp[i-2][1]%k+dp[i-2][3]%k
+    dp[i][3] = dp[i-3][1]%k+dp[i-3][2]%k
+
+for i in num_lst:
+    print((dp[i][1]+dp[i][2]+dp[i][3])%k)
