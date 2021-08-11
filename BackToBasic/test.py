13d56d848aabc67e57ea3b23b346fe83d736c191
osj3474 <handong3474@gmail.com>
2021-08-11 17:59:59 +0900

    Update 15651

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -190,3 +190,18 @@
 #
 #
 # print(MAX)
+
+N, M = map(int, input().split())
+A = [0]*(N+1)
+
+def go(idx, n, m):
+    if idx==m:
+        for i in range(m):
+            print(A[i], end=' ')
+        print()
+        return
+    for i in range(1, n+1):
+        A[idx] = i
+        go(idx+1, n, m)
+
+go(0, N, M)
