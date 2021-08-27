127e402bdd57ea59ff1eb18f79e6233b6d8e3234
osj3474 <handong3474@gmail.com>
2021-08-27 23:30:54 +0900

    Update 16926

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -4,14 +4,25 @@ def rotate(board, n):
     temp = [[0]*n for _ in range(n)]
     for x in range(n//2):
         N = x+n-1
-        for i in range(1, n):
+        print(x, N)
+        for i in range(x+1, N+1):
+            print(i, x, N)
             temp[i][x] = board[i-1][x]
-            temp[x][i] = board[N][i-1]
-            temp[i][N] = board[i+1][N]
-            temp[x][i] = board[x][i+1]
+            temp[N][i] = board[N][i-1]
+            temp[i-1][N] = board[i][N]
+            temp[x][i-1] = board[x][i]
         n-=2
     return temp
 
 
-N, M, R = map(int, input().split())
-board = [[0]*M for _ in range(N)]
+# N, M, R = map(int, input().split())
+# board = [[0]*M for _ in range(N)]
+
+board = [[1,2,3,4],
+         [5,6,7,8],
+         [9,10,11,12],
+         [13,14,15,16]]
+
+res = rotate(board, 4)
+for r in res:
+    print(r)
