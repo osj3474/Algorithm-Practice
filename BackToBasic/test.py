2b35d957e74ec0675f8c67108e4ffd46329197c4
osj3474 <handong3474@gmail.com>
2021-09-01 22:18:08 +0900

    Update input case

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -1,28 +1,35 @@
 # 16926
 
-def rotate(board, n):
-    temp = [[0]*n for _ in range(n)]
-    for x in range(n//2):
+def rotate(board, n, m):
+    temp = [[0]*m for _ in range(n)]
+    MIN = min(n, m)
+    for x in range(MIN//2):
         N = x+n-1
-        print(x, N)
+        M = x+m-1
+        # print(x, N)
         for i in range(x+1, N+1):
-            print(i, x, N)
+            # print(i, x, N)
             temp[i][x] = board[i-1][x]
             temp[N][i] = board[N][i-1]
             temp[i-1][N] = board[i][N]
             temp[x][i-1] = board[x][i]
+        for i in range(x+1, M+1):
+            print()
         n-=2
     return temp
 
 
 # N, M, R = map(int, input().split())
-# board = [[0]*M for _ in range(N)]
+# board = [list(map(int ,input().split())) for _ in range(N)]
 
-board = [[1,2,3,4],
-         [5,6,7,8],
-         [9,10,11,12],
-         [13,14,15,16]]
+N, M, R = 2, 3, 2
+board = [[1,2,3], [4,5,6]]
 
-res = rotate(board, 4)
-for r in res:
-    print(r)
+for _ in range(R):
+    board = rotate(board, N, M)
+
+# for i in range(N):
+#     for j in range(M):
+#         print(board[i][j], end=' ')
+#     print()
+print(board)
