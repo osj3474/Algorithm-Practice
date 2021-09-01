bc082ef125c0b702cfa4328998ae12531deb47e6
osj3474 <handong3474@gmail.com>
2021-09-02 08:41:10 +0900

    Try one cycle

--- BackToBasic/test.py
+++ BackToBasic/test.py
@@ -1,32 +1,32 @@
-# 16926
-
-def rotate(board, n, m):
+def rotate(board, n, m, cnt):
     temp = [[0]*m for _ in range(n)]
     MIN = min(n, m)
-    for x in range(MIN//2):
-        N = x+n-1
-        M = x+m-1
-        # print(x, N)
-        for i in range(x+1, N+1):
-            # print(i, x, N)
-            temp[i][x] = board[i-1][x]
-            temp[N][i] = board[N][i-1]
-            temp[i-1][N] = board[i][N]
-            temp[x][i-1] = board[x][i]
-        for i in range(x+1, M+1):
-            print()
-        n-=2
+    N = n - 1
+    M = m - 1
+    idx_lst = [0] * (n * m)
+    for x in range(MIN//2):   # 10^8
+        idx = 0
+        # for i in range(x, n):
+        #     idx_lst[idx] = (i, x)
+        #     idx += 1
+        # for i in range(x, m):
+        #     idx_lst[idx] = (N, i)
+        #     idx += 1
+        # for i in range(M, -1, -1):
+        #     idx_lst[idx] = (i, M)
+        #     idx += 1
+        # for i in range(M-1, 0, -1):
+        #     idx_lst[idx] = (x, i)
+
+        N -= 1
+        M -= 1
     return temp
 
+N, M, R = map(int, input().split())
+board = [list(map(int ,input().split())) for _ in range(N)]
 
-# N, M, R = map(int, input().split())
-# board = [list(map(int ,input().split())) for _ in range(N)]
-
-N, M, R = 2, 3, 2
-board = [[1,2,3], [4,5,6]]
-
-for _ in range(R):
-    board = rotate(board, N, M)
+# for _ in range(R):
+board = rotate(board, N, M, R)
 
 # for i in range(N):
 #     for j in range(M):
