e8c3587d71912dcb64d18b77f9b46643bd9b4fca
osj3474 <handong3474@gmail.com>
2021-09-06 20:37:42 +0900

    Update 3085

--- BackToBasic/baek3085.py
+++ BackToBasic/baek3085.py
@@ -1,33 +1,33 @@
-from collections import defaultdict
-
 N = int(input())
-A = [[0]*N for _ in range(N)]
-for i in range(N):
-    string = input()
-    for j in range(N):
-        A[i][j] = string[j]
+board = [list(input()) for _ in range(N)]
+
+def find_max_value(lst):
+    M, cnt = 1, 1
+    for i in range(1, N):
+        if lst[i-1] == lst[i]:
+            cnt += 1
+            if M<cnt: M=cnt
+        else:
+            cnt = 1
+    return M
+
+MAX = 1
 
 dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+dy = [0, -1, 0, 1]
 
-MAX = 0
 for i in range(N):
     for j in range(N):
+        cur = board[i][j]
         for p in range(4):
             x = i + dx[p]
             y = j + dy[p]
-            if x<0 or x>=N or y<0 or y>=N: continue
-            if A[i][j] != A[x][y]:
-                line_lst = [defaultdict(int) for _ in range(4)]
-                A[i][j], A[x][y] = A[x][y], A[i][j]
-                for k in range(N):
-                    line_lst[0][A[i][k]] += 1
-                    line_lst[1][A[x][k]] += 1
-                    line_lst[2][A[k][j]] += 1
-                    line_lst[3][A[k][y]] += 1
-                for k in range(4):
-                    temp = max(line_lst[k].items())[1]
-                    if MAX < temp:
-                        MAX = temp
-                A[x][y], A[i][j] = A[i][j], A[x][y]
+            if x>=0 and y>=0 and x<N and y<N:
+                if cur != board[x][y]:
+                    board[i][j], board[x][y] = board[x][y], board[i][j]
+                    vert_lst = list(map(list, list(zip(*board))))
+                    for k in range(N):
+                        MAX = max(MAX, find_max_value(board[k]), find_max_value(vert_lst[k]))
+                    board[i][j], board[x][y] = board[x][y], board[i][j]
+
 print(MAX)
