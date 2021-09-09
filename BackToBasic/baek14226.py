abf151bd48b81bda3c566d8195e9eea22d640abd
osj3474 <handong3474@gmail.com>
2021-09-09 21:27:50 +0900

    Update 14226

--- BackToBasic/baek14226.py
+++ BackToBasic/baek14226.py
@@ -1,6 +1,22 @@
-# 파이썬은 recursion depth가 작구나
-# 탐색은 반복문으로 가는걸로
-# DS : DFS는 Stack
-# BQ : BFS는 Queue
-# 추후 DP를 위한 memo 리스트도 넣자
+from collections import deque
 
+S = int(input())
+
+need, new, visited = deque(), list(), set()
+need.append((1, 0))
+cnt = 0
+
+while need:
+    v, c = need.popleft()
+    if v == S: break
+    if v in visited: continue
+    for k in [(v, v), (v+c, c), (v-1, c)]:
+        if k[0]<0 or k[0]>2000 or k in visited: continue
+        new.append(k)
+        visited.add(k)
+    if not need:
+        cnt += 1
+        need.extend(new)
+        new.clear()
+
+print(cnt)
