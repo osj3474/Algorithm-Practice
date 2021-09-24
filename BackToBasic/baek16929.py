297a4d49f806fd7a2706578155c4e5341cb58517
osj3474 <handong3474@gmail.com>
2021-09-24 20:10:30 +0900

    Update 16929

--- BackToBasic/baek16929.py
+++ BackToBasic/baek16929.py
@@ -1,3 +1,5 @@
+# 통과했음.
+
 N, M = map(int, input().split())
 board = [list(input()) for _ in range(N)]
 
@@ -35,12 +37,10 @@ for l in range(N):
 
             if cnt==0 and need:
                 k = idx_lst.pop()
-                # print('다시 갈림길로 돌아가야 해 => {}를 향해!'.format(k))
                 for v in range(len(visited)-1, -1, -1):
                     if visited[v] == k:
                         break
                     visited.pop()
-                # print('visited = {}'.format(visited))
             if fin: break
         if fin: break
     if fin: break
@@ -49,3 +49,37 @@ if fin:
     print('Yes')
 else:
     print('No')
+
+
+# 백준 정답
+# dx = [0,0,1,-1]
+# dy = [1,-1,0,0]
+# n,m = map(int,input().split())
+# a = [input() for _ in range(n)]
+# dist = [[0]*m for _ in range(n)]
+# check = [[False]*m for _ in range(n)]
+# def go(x, y, cnt, color):
+#     if check[x][y]:
+#         if cnt-dist[x][y] >= 4:
+#             return True
+#         else:
+#             return False
+#     check[x][y] = True
+#     dist[x][y] = cnt
+#     for k in range(4):
+#         nx,ny = x+dx[k],y+dy[k]
+#         if 0 <= nx < n and 0 <= ny < m:
+#             if a[nx][ny] == color:
+#                 if go(nx,ny,cnt+1,color):
+#                     return True
+#     return False
+# for i in range(n):
+#     for j in range(m):
+#         if check[i][j]:
+#             continue
+#         dist = [[0]*m for _ in range(n)]
+#         ok = go(i, j, 1, a[i][j])
+#         if ok:
+#             print('Yes')
+#             exit()
+# print('No')
